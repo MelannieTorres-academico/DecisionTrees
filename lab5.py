116a66f29d9e5acb8180d97ed9f9329778b352d7
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-10-23 19:07:22 -0500

    wip parser - separate header and data

--- lab5.py
+++ lab5.py
@@ -1,13 +1,37 @@
 import fileinput
+relation = ""
+attribute = {}
+
+
+def format_header(header):
+    print(header)
+    return
+
+
+def format_data(data):
+    print(data)
+    return
 
 
 def main():
     # example input
     file_input = fileinput.input()
-    line = []
+    lines = []
     for x in file_input:
         if x[0] != '%':
-            line.append(x)
+            lines.append(x)
+
+    input_len = len(lines)
+    data_index = 0
+    for i in range(0, input_len):
+        header = lines[i].split(" ")[0]
+        if "@data" not in header:
+            format_header(lines[i])
+        else:
+            data_index = i
+            break
+
+    format_data(lines[data_index + 1:input_len])
 
 
 if __name__ == "__main__":
