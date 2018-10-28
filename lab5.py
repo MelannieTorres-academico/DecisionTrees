5ad111e7f7bede4221ce0ff4e44a02b1c980979a
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-10-28 14:58:48 -0600

    structured recursive function, refactored code, missing formating output

--- lab5.py
+++ lab5.py
@@ -6,50 +6,49 @@ import numpy as np
 relation = ""
 attributes = {}
 
+
 # Calculates the entropy
 # dataframe= rows that should be taken into accoutn
-# result name of the column name of the output (ex. play)
-def calculate_entropy(dataframe, result):
-    occurrences = count_occurrences(dataframe, result)
-    total_elements=dataframe.shape[0]
+def calculate_entropy(dataframe):
+    occurrences = count_occurrences(dataframe)
+    total_elements = dataframe.shape[0]
     result = 0
-    for occurrence in occurrences:
-        element=occurrences[occurrence]
-        result = result - (element / total_elements * math.log2(element / total_elements))
-    #print("Entropy: {0}".format(result))
+    for key in occurrences:
+        elem_occurrences = occurrences[key]
+        result = result - (elem_occurrences / total_elements * math.log2(elem_occurrences / total_elements))
+    # print("Entropy: {0}".format(result))
     return result
 
 
 # Calculates the entropy of the given possible child
 # dataframe: full dataframe
 # headers without including the outputs
-# outputs name of the column with the possible outputs (ex: play)
-def calculate_entropy_TX(dataframe, p, outputs):
+def calculate_entropy_TX(dataframe, p):
     total_rows = dataframe.shape[0]
-    result=0
+    result = 0
     filtered_rows = dataframe[p].value_counts().to_dict()
     for option in filtered_rows:
-        #filter rows where equal to first possibility (p) and call calculate_entropy
-        aux_entropy = calculate_entropy(dataframe[dataframe[p] == option], outputs)
+        # filter rows where equal to first possibility (p) and call calculate_entropy
+        aux_entropy = calculate_entropy(dataframe[dataframe[p] == option])
         aux_row_count = dataframe[dataframe[p] == option].shape[0]
         result = result+(aux_row_count/total_rows*aux_entropy)
-    #print("Entropy {0}: {1}".format(option, result))
+    # print("Entropy {0}: {1}".format(option, result))
     return result
+
 
 # Calculates the information gain
 #
-def calculate_information_gain(dataframe, p, outputs, current_entropy):
-    entropy_tx = calculate_entropy_TX(dataframe, p, outputs)
-    return current_entropy-entropy_tx
+def calculate_information_gain(dataframe, p, current_entropy):
+    entropy_tx = calculate_entropy_TX(dataframe, p)
+    return current_entropy - entropy_tx
+
 
 # Stores in a dictionary how many times each value of the result occurs in the given part of the dataframe
 # Output example {"yes":9, "no":4}
-def count_occurrences(dataframe, result):
-    possible_attributes = attributes[result]
-    occurrences = []
-    total_count = dataframe[result].value_counts()
-    return total_count.to_dict()
-
+def count_occurrences(dataframe):
+    y_column_name = list(attributes.keys())[-1]
+    total_count = dataframe[y_column_name].value_counts().to_dict()
+    return total_count
 
 
 def format_header(header):
@@ -73,8 +72,33 @@ def format_data(data):
     return dataframe
 
 
-def generate_tree_model(dataframe, attributes):
-    # ...
+def same_y_values(dataframe):
+    y_values = len(count_occurrences(dataframe))
+    return y_values == 1
+
+
+def generate_tree_model(dataframe):
+    print("\n\n generate_tree_model() function call: \n data: ")
+    print(dataframe)
+    if same_y_values(dataframe):
+        print("all y values are the same, entropy is 0")
+        return
+    x_headers = list(dataframe.columns.values)[:-1]  # get x headers (ex. outlook temperature humidity  windy play)
+    current_entropy = calculate_entropy(dataframe)
+
+    max_information_gain = 0
+    max_x_col = ""
+    # calculate Information gain of each possible child
+    for x_column in x_headers:
+        information_gain = calculate_information_gain(dataframe, x_column, current_entropy)
+        # select the column with the maximum information gain:
+        if information_gain > max_information_gain:
+            max_information_gain = information_gain
+            max_x_col = x_column
+    print("split on column: {0}, information gain: {1}".format(max_x_col, max_information_gain))
+    # for each attribute value, split dataframe:
+    for value in attributes[max_x_col]:
+        generate_tree_model(dataframe[dataframe[max_x_col] == value])
     return
 
 
@@ -96,22 +120,13 @@ def main():
         elif "@data" in header:
             data_index = i
             break
-    # print("attributes dictionary:")
-    # print(attributes)
+    print("attributes dictionary:")
+    print(attributes)
     dataframe = format_data(lines[data_index + 1:input_len])
     print("\nfull dataframe:")
     print(dataframe)
 
-
-    headers = list(dataframe.columns.values) # get the headers (ex. outlook temperature humidity  windy play)
-    result = headers[-1] # get the last value of the headers (ex. play)
-    current_entropy = calculate_entropy(dataframe, result)
-
-    #calculate Information gain of each possible child
-    for possibility in headers[:-1]:
-        information_gain = calculate_information_gain(dataframe, possibility, result, current_entropy)
-        print("Information gain: {0}".format(information_gain))
-
+    generate_tree_model(dataframe)
 
 
 if __name__ == "__main__":
